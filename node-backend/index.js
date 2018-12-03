915b569ad7745584ac9be3211d0e5e6c334457f5
Aparna Dalal <34259886+aparnada7@users.noreply.github.com>
2018-12-03 09:25:12 -0800

    handling routes for api calls search/update/delete node
    handling search/update/delete in BE

--- node-backend/index.js
+++ node-backend/index.js
@@ -38,6 +38,9 @@ const deleteSensor = require('./routes/deleteSensor')
 const updateSensor = require('./routes/updateSensor')
 const addNode = require('./routes/addNode')
 const addCluster = require('./routes/addCluster')
+const getNode = require('./routes/getNode')
+const updateNode = require('./routes/updateNode')
+const deleteNode = require('./routes/deleteNode')
 
 //use cors to allow cross origin resource sharing
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
@@ -94,6 +97,9 @@ app.post('/deleteSensor', deleteSensor)
 app.post('/updateSensor', updateSensor)
 app.post('/addNode', addNode)
 app.post('/addCluster', addCluster)
+app.post('/getNode', getNode)
+app.post('/updateNode', updateNode)
+app.post('/deleteNode', deleteNode)
 
 app.listen(3001);
 console.log("Server Listening on port 3001");
